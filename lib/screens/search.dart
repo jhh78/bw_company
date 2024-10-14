82783071e338010bc19b1c707578ebb70c13d31f
鄭會憲 <vkcel400@gmail.com>
2024-10-14 09:29:11 +0900

    검색페이지 좌측메뉴 분리

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/providers/search_screen.dart';
-import 'package:flutter_application_1/screens/help.dart';
-import 'package:flutter_application_1/screens/notice.dart';
-import 'package:flutter_application_1/screens/support.dart';
 import 'package:flutter_application_1/widgets/common/bottom_loading.dart';
+import 'package:flutter_application_1/widgets/common/side_menu.dart';
 import 'package:flutter_application_1/widgets/search/list_contents.dart';
 import 'package:flutter_application_1/widgets/search/search_input_form.dart';
 import 'package:get/get.dart';
@@ -59,68 +57,7 @@ class SearchScreenState extends State<SearchScreen> {
           title: Text("searchListTitle".tr),
           centerTitle: true,
         ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              SizedBox(
-                height: 100,
-                child: DrawerHeader(
-                  decoration: const BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                  child: Text(
-                    'naviMenu'.tr,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.search),
-                title: Text('naviSearch'.tr),
-                onTap: () {
-                  Get.offAll(
-                    () => const SearchScreen(),
-                    transition: Transition.fade,
-                  );
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.info_outline),
-                title: Text('naviNotification'.tr),
-                onTap: () {
-                  Get.to(
-                    () => const NoticeScreen(),
-                    transition: Transition.fade,
-                  );
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.help_outline),
-                title: Text('naviHelp'.tr),
-                onTap: () {
-                  Get.to(
-                    () => const HelpScreen(),
-                    transition: Transition.fade,
-                  );
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.payment),
-                title: Text('naviSupport'.tr),
-                onTap: () {
-                  Get.to(
-                    () => const SupportScreen(),
-                    transition: Transition.fade,
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
+        drawer: const SideMenu(),
         body: Container(
           padding: const EdgeInsets.all(20),
           color: Colors.white,
